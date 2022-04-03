af8fe6d9aa4a4fb06e2dc8e6bf13c35b6e850be9
Andy <andygangwenh8880@hstat.org>
2022-04-03 18:21:35 -0400

    Skeleton of App Complete

--- app/src/main/java/com/jianpingandy/freedomproject/CreateAccount.java
+++ app/src/main/java/com/jianpingandy/freedomproject/CreateAccount.java
@@ -2,7 +2,11 @@ package com.jianpingandy.freedomproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        MaterialButton returnHome = (MaterialButton) findViewById(R.id.returnHome);
+        MaterialButton login = (MaterialButton) findViewById(R.id.makeaccount);
+
+        returnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(getApplicationContext(),Main_Page.class);
+                startActivity(login);
+            }
+        });
     }
 }
--- app/src/main/java/com/jianpingandy/freedomproject/Main_Page.java
+++ app/src/main/java/com/jianpingandy/freedomproject/Main_Page.java
@@ -2,7 +2,11 @@ package com.jianpingandy.freedomproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Main_Page extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Main_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        MaterialButton loginpage = (MaterialButton) findViewById(R.id.returnHome);
+
+        loginpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginPage = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(loginPage);
+            }
+        });
     }
 }
