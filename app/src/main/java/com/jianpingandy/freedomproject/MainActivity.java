a1fa6aa1618a20f08b311ee53b28bdd63b112938
Andy <andygangwenh8880@hstat.org>
2022-04-03 16:37:53 -0400

    Login Page Design Complete Login Btn goes to main page

--- app/src/main/java/com/jianpingandy/freedomproject/MainActivity.java
+++ app/src/main/java/com/jianpingandy/freedomproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.jianpingandy.freedomproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                    startActivity(intent);
                 }else{
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                 }
