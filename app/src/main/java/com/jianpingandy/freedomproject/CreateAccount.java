a23d18bcfc2e7aeb1bb2c68a31b66f9355095a57
alanc <alanchen8647@gmail.com>
2022-04-23 17:57:12 -0400

    Step 2: create account function

--- app/src/main/java/com/jianpingandy/freedomproject/CreateAccount.java
+++ app/src/main/java/com/jianpingandy/freedomproject/CreateAccount.java
@@ -4,17 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccount extends AppCompatActivity {
     FirebaseAuth mAuth;
@@ -27,7 +37,6 @@ public class CreateAccount extends AppCompatActivity {
         MaterialButton returnHome = (MaterialButton) findViewById(R.id.returnHome);
         MaterialButton login = (MaterialButton) findViewById(R.id.makeaccount);
         mAuth = FirebaseAuth.getInstance();
-
         login.setOnClickListener(view -> {
             createUser();
         });
@@ -43,6 +52,9 @@ public class CreateAccount extends AppCompatActivity {
         EditText newPassword = findViewById(R.id.newPassword);
         String email = newUsername.getText().toString();
         String password = newPassword.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
 
         if(TextUtils.isEmpty(email)) {
             newUsername.setError("Email cannot be empty");
@@ -55,7 +67,12 @@ public class CreateAccount extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("email",email);
+                        user.put("password",password);
+                        user.put("username","visitor");
                         Toast.makeText(CreateAccount.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        db.collection("User").document(email).set(user);
                         startActivity(new Intent(CreateAccount.this,Main_Page.class));
                     } else {
                         Toast.makeText(CreateAccount.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/jianpingandy/freedomproject/ProfileFragment.java
+++ app/src/main/java/com/jianpingandy/freedomproject/ProfileFragment.java
@@ -1,19 +1,42 @@
 package com.jianpingandy.freedomproject;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
+
 }
